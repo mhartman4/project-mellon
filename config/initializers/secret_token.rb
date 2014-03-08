2eb5b0cf3e2195ff698f0ff250e849e1c75db138
Michael Hartman <michael@orderup.com>
2014-03-08 11:38:43 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectMellon::Application.config.secret_key_base = '1bb40b89720b4ff35a20d5fe019a70e25acd4cff23819e816fe34e63a7ef43421eafe4b180c07241fd094912d4309e73274d509ecb419a81ac44c9caa5e9efa6'
