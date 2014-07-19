79cc6a036b061c06019a82bbc26b0b174b5ae695
Michael Hartman <michael@orderup.com>
2014-07-19 11:23:42 -0400

    linear regression startin to look real

--- app/controllers/linear_regression_controller.rb
+++ app/controllers/linear_regression_controller.rb
@@ -1,6 +1,8 @@
 class LinearRegressionController < ApplicationController
 
   def index
-    #@games = Game.all
+    @game_data = Game.all.pluck(params[:x], params[:y])
+    @x = @game_data.collect {|point| point[0] }
+    @y = @game_data.collect {|point| point[1] }
   end
 end
